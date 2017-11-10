c77abdb8b5c585c84409fe898c4209b5be450394
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-10 16:49:21 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,7 +2,12 @@ class LandmarksController < ApplicationController
 
   get '/landmarks' do
     @landmarks = Landmark.all
-    
+
     erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+
+  erb :'landmarks/new'
   end
 end
