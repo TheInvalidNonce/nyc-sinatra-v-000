ff7e4557a604e2b0258ddd741e6fbb2374759cb6
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-10 17:05:45 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,17 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
+    @figures = Figure.all
+
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+
+    erb :'figures/new'
+  end
 
   
-  end
 end
