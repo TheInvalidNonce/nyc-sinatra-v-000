edf23c7773e3e997a6ce7674080c7c9507d76a21
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-18 22:31:41 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,11 +1,5 @@
 class FiguresController < ApplicationController
 
-  get '/figures' do
-    @figures = Figure.all
-
-    erb :'figures/index'
-  end
-
   get '/figures/new' do
     @landmarks = Landmark.all
     @titles = Title.all
@@ -13,30 +7,47 @@ class FiguresController < ApplicationController
     erb :'figures/new'
   end
 
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
   post '/figures' do
     @figure = Figure.create(params[:figure])
 
-    if !params[:title].empty?
+    if !params[:title][:name].empty?
       @figure.titles << Title.create(params[:title])
     end
-    if !params[:landmark].empty?
+    if !params[:landmark][:name].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
+
+    @figure.save
 
     redirect "figures/#{@figure.id}"
   end
 
   get '/figures/:id' do
-    @figure = Figure.find_by_id(params[:id])
+    @figure = Figure.find(params[:id])
 
     erb :'figures/show'
   end
 
   get '/figures/:id/edit' do
-    @figure = Figure.find_by_id(params[:id])
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
 
     erb :'figures/edit'
   end
 
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    @figure.save
+
+    redirect "figures/#{@figure.id}"
+  end
 
 end
